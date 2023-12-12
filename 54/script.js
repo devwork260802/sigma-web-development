9c6f9ee024bd258c1e09871a587d70386c103749
Dev <devwork260802@gmail.com>
2023-12-12 16:52:51 +0530

    Dev

---
+++ 54/script.js
@@ -0,0 +1,13 @@
+alert("Hello");
+
+console.log('Hello Everyone');
+
+let number = prompt("Enter Number");
+let isTrue = confirm("Please Select");
+if (isTrue){
+    console.log('Yes');
+}
+else{
+    console.log('No');
+}
+console.log('Your Number is ' + number);
