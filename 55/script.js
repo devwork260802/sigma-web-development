eb193fd3b83c9f1a01756c1469db180fc79e4db8
Dev <devwork260802@gmail.com>
2023-12-13 18:15:48 +0530

    Dev

---
+++ 55/script.js
@@ -0,0 +1,17 @@
+console.log('Hello Everyone');
+
+var a=5;
+var b=6;
+
+let c=7;
+
+console.log(a+b);
+console.log(c);
+console.log(typeof a);
+
+let me={
+    "name": "Dev Gupta",
+    "role": "Developer and Editor"
+}
+
+console.log(me);
